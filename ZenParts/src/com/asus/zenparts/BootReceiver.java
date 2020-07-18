1d8ead8938240071bf6935790f44a16d79670086
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-18 09:25:24 +0530

    X00T: ZenParts: Ported Dirac Service From XiaomiParts
    Signed-off-by: mprabhat774 <mprabhat774@gmail.com>
    
    - Added Dirac QS Tile
    Change-Id: I9c42ffc42c4d3625062c49a191e8fa41c7d0df06
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- ZenParts/src/com/asus/zenparts/BootReceiver.java
+++ ZenParts/src/com/asus/zenparts/BootReceiver.java
@@ -51,6 +51,9 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
         FileUtils.setValue(DeviceSettings.BACKLIGHT_DIMMER_PATH, Settings.Secure.getInt(context.getContentResolver(),
                 DeviceSettings.PREF_BACKLIGHT_DIMMER, 0));
 
+        //Dirac
+        context.startService(new Intent(context, DiracService.class));
+
         if (Settings.Secure.getInt(context.getContentResolver(), PREF_ENABLED, 0) == 1) {
             FileUtils.setValue(KCAL_ENABLE, Settings.Secure.getInt(context.getContentResolver(),
                     PREF_ENABLED, 0));
